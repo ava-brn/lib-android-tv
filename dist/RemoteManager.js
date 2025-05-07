5c13f9714768b358848c64c10d2b9dac36df0fc6
Emaro <ma@ava.com>
2025-05-07 10:58:18 +0200

    Only add raw listener once

--- dist/RemoteManager.js
+++ dist/RemoteManager.js
@@ -11,77 +11,79 @@ class RemoteManager extends Client_1.Client {
         this.remoteMessageManager = new RemoteMessageManager_1.RemoteMessageManager();
     }
     async connect() {
-        this.on('raw', (buffer) => {
-            const message = this.remoteMessageManager.parse(buffer);
-            if (!message.remotePingRequest) {
-                this.emit('message', JSON.stringify(message));
-            }
-            if (message.remoteConfigure) {
-                this.socket?.write(this.remoteMessageManager.createRemoteConfigure(622, 'Build.MODEL', 'Build.MANUFACTURER', 1, 'Build.VERSION.RELEASE'));
-                this.emit('ready');
-            }
-            else if (message.remoteSetActive) {
-                this.socket?.write(this.remoteMessageManager.createRemoteSetActive(622));
-            }
-            else if (message.remotePingRequest) {
-                this.socket?.write(this.remoteMessageManager.createRemotePingResponse(message.remotePingRequest.val1));
-            }
-            else if (message.remoteImeKeyInject) {
-                this.emit('current_app', message.remoteImeKeyInject.appInfo.appPackage);
-            }
-            else if (message.remoteImeBatchEdit) {
-                this.emit('log', 'Receive IME BATCH EDIT' + message.remoteImeBatchEdit);
-            }
-            else if (message.remoteImeShowRequest) {
-                this.emit('log', 'Receive IME SHOW REQUEST' + message.remoteImeShowRequest);
-            }
-            else if (message.remoteVoiceBegin) {
-                //this.emit('log.debug', "Receive VOICE BEGIN" + message.remoteVoiceBegin);
-            }
-            else if (message.remoteVoicePayload) {
-                //this.emit('log.debug', "Receive VOICE PAYLOAD" + message.remoteVoicePayload);
-            }
-            else if (message.remoteVoiceEnd) {
-                //this.emit('log.debug', "Receive VOICE END" + message.remoteVoiceEnd);
-            }
-            else if (message.remoteStart) {
-                this.emit('powered', message.remoteStart.started);
-            }
-            else if (message.remoteSetVolumeLevel) {
-                this.emit('volume', {
-                    level: message.remoteSetVolumeLevel.volumeLevel,
-                    maximum: message.remoteSetVolumeLevel.volumeMax,
-                    muted: message.remoteSetVolumeLevel.volumeMuted,
-                });
-                //this.emit('log.debug', "Receive SET VOLUME LEVEL" + message.remoteSetVolumeLevel.toJSON().toString());
-            }
-            else if (message.remoteSetPreferredAudioDevice) {
-                //this.emit('log.debug', "Receive SET PREFERRED AUDIO DEVICE" + message.remoteSetPreferredAudioDevice);
-            }
-            else if (message.remoteError) {
-                if (message.remoteError?.message?.remoteConfigure) {
-                    this.emit('unpaired', message.remoteError);
-                }
-                else {
-                    this.emit('log', "Receive REMOTE ERROR");
-                    this.emit('error', message.remoteError);
-                }
-            }
-            else if (message.remoteKeyInject) {
-                this.emit('key', message.remoteKeyInject);
-            }
-            else {
-                this.emit('log', 'What else ?');
-            }
-        });
-        console.log('RAW LISTENERS', this.listenerCount('raw'));
-        this.socket?.on('timeout', () => {
-            this.emit('log', 'timeout');
+        if (this.listenerCount('raw') < 1) {
+            this.on('raw', (buffer) => this.handleRawMessage(buffer));
+        }
+        this.socket?.once('timeout', () => {
+            this.emit('log', `Connection to [${this.host}] timed out.`);
             this.socket?.destroy();
         });
         // Ping is received every 5 seconds
         this.socket?.setTimeout(this.connectionTimeout);
         return await super.connect();
+    }
+    handleRawMessage(buffer) {
+        const message = this.remoteMessageManager.parse(buffer);
+        if (!message.remotePingRequest) {
+            this.emit('message', JSON.stringify(message));
+        }
+        if (message.remoteConfigure) {
+            this.socket?.write(this.remoteMessageManager.createRemoteConfigure(622, 'Build.MODEL', 'Build.MANUFACTURER', 1, 'Build.VERSION.RELEASE'));
+            this.emit('ready');
+        }
+        else if (message.remoteSetActive) {
+            this.socket?.write(this.remoteMessageManager.createRemoteSetActive(622));
+        }
+        else if (message.remotePingRequest) {
+            this.socket?.write(this.remoteMessageManager.createRemotePingResponse(message.remotePingRequest.val1));
+        }
+        else if (message.remoteImeKeyInject) {
+            this.emit('current_app', message.remoteImeKeyInject.appInfo.appPackage);
+        }
+        else if (message.remoteImeBatchEdit) {
+            this.emit('log', 'Receive IME BATCH EDIT' + message.remoteImeBatchEdit);
+        }
+        else if (message.remoteImeShowRequest) {
+            this.emit('log', 'Receive IME SHOW REQUEST' + message.remoteImeShowRequest);
+        }
+        else if (message.remoteVoiceBegin) {
+            //this.emit('log.debug', "Receive VOICE BEGIN" + message.remoteVoiceBegin);
+        }
+        else if (message.remoteVoicePayload) {
+            //this.emit('log.debug', "Receive VOICE PAYLOAD" + message.remoteVoicePayload);
+        }
+        else if (message.remoteVoiceEnd) {
+            //this.emit('log.debug', "Receive VOICE END" + message.remoteVoiceEnd);
+        }
+        else if (message.remoteStart) {
+            this.emit('powered', message.remoteStart.started);
+        }
+        else if (message.remoteSetVolumeLevel) {
+            this.emit('volume', {
+                level: message.remoteSetVolumeLevel.volumeLevel,
+                maximum: message.remoteSetVolumeLevel.volumeMax,
+                muted: message.remoteSetVolumeLevel.volumeMuted,
+            });
+            //this.emit('log.debug', "Receive SET VOLUME LEVEL" + message.remoteSetVolumeLevel.toJSON().toString());
+        }
+        else if (message.remoteSetPreferredAudioDevice) {
+            //this.emit('log.debug', "Receive SET PREFERRED AUDIO DEVICE" + message.remoteSetPreferredAudioDevice);
+        }
+        else if (message.remoteError) {
+            if (message.remoteError?.message?.remoteConfigure) {
+                this.emit('unpaired', message.remoteError);
+            }
+            else {
+                this.emit('log', "Receive REMOTE ERROR");
+                this.emit('error', message.remoteError);
+            }
+        }
+        else if (message.remoteKeyInject) {
+            this.emit('key', message.remoteKeyInject);
+        }
+        else {
+            this.emit('log', 'What else ?');
+        }
     }
     sendPower() {
         this.socket?.write(this.remoteMessageManager.createRemoteKeyInject(this.remoteMessageManager.RemoteDirection.SHORT, this.remoteMessageManager.RemoteKeyCode.KEYCODE_POWER));
